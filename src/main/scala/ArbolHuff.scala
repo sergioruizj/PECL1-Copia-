1a89d6dc1213af0abc72ac52c046773262313066
sergioruizj <sergio.ruizj@edu.uah.es>
2024-10-25 09:14:33 +0200

    25/10

--- src/main/scala/ArbolHuff.scala
+++ src/main/scala/ArbolHuff.scala
@@ -1,5 +1,21 @@
 abstract class ArbolHuff {
 
+  def peso(arbol: ArbolHuff): Int = arbol match
+    case NodoHuff(caracter, frecuencia) => frecuencia
+    case RamaHuff(nodoDch, nodoIzq) => peso(nodoIzq) + peso(nodoDch)
+
+  def caracteres(arbol: ArbolHuff): List[Char] =
+    def caracteresAux(arbol: ArbolHuff, Lista: List[Char]): List[Char] = arbol match
+      case NodoHuff(caracter, frecuencia) => caracter :: Lista
+      case RamaHuff(nodoDch, nodoIzq) => caracteresAux(nodoIzq, Lista) ::: caracteresAux(nodoDch, Lista)
+
+    caracteresAux(arbol, Nil)
+
+  def cadenaAListaChars(cadena: String): List[Char] =
+    cadena.toList
+
+  def listaCharsACadena(listaChar: List[Char]): String =
+    listaChar.mkString
 }
 
 
